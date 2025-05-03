bd4bef6afba32a3761d7efaeea0bdfeb2d65ec5b
yungflashh <streaktv8@gmail.com>
2025-05-03 21:57:43 +0100

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,54 @@
 import { Tabs } from 'expo-router';
+import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          let iconName;
+
+          if (route.name === 'index') {
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'saved') {
+            iconName = focused ? 'book' : 'book';
+          } else if (route.name === 'search') {
+            iconName = focused ? 'search1' : 'search1';
+          } else if (route.name === 'profile') {
+            iconName = focused ? 'user' : 'user';
+          }
+
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+        tabBarItemStyle : {
+            width: "100%",
+            height : "100%",
+            justifyContent: "center",
+            alignItems: "center",
+
+        },
+        tabBarStyle: {
+            backgroundColor: "#0f0D23",
+            borderRadius: 50,
+            marginHorizontal: 10,
+            marginBottom: 20,
+            position: "absolute",
+          
+        },
+
+
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: "white",
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      })}
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="saved" options={{ title: "Saved" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
-}
+};
+
+export default _layout;
